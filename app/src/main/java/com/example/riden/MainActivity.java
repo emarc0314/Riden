4362cadb01c21f8e1018d3b089f90ca45277eb39
Elvis Marcelo <75585259+emarc0314@users.noreply.github.com>
2022-06-20 13:43:03 -0700

    Renamed project

--- app/src/main/java/com/example/riden/MainActivity.java
+++ app/src/main/java/com/example/riden/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.riden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/test/java/com/example/riden/ExampleUnitTest.java
+++ app/src/test/java/com/example/riden/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.riden;
 
 import org.junit.Test;
 
