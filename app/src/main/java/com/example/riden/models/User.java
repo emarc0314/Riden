9a2a860b77871d9f897b3fb9eda257e6024f3da0
Elvis Marcelo <75585259+emarc0314@users.noreply.github.com>
2022-06-21 09:30:52 -0700

    most up to date files

--- app/src/main/java/com/example/riden/models/User.java
+++ app/src/main/java/com/example/riden/models/User.java
@@ -1,2 +1,23 @@
-package com.example.riden.models;public class User {
+package com.example.riden.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("User")
+public class User extends ParseObject {
+    public static final String USERNAME_KEY = "username";
+    public static final String PHONENUMBER_KEY = "phoneNumber";
+
+    public String getUsername() {
+        return getString(USERNAME_KEY);
+    }
+    public void setUsername(String username) {
+        put(USERNAME_KEY, username);
+    }
+    public String getPhoneNumber() {
+        return getString(PHONENUMBER_KEY);
+    }
+    public void setPhoneNumber(String number) {
+        put(PHONENUMBER_KEY, number);
+    }
 }
