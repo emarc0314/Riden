33b2445c47359f16bc17d11f4bed358f1827c64d
Elvis Marcelo <75585259+emarc0314@users.noreply.github.com>
2022-06-22 11:52:41 -0700

    Profile Fragment

--- app/src/main/java/com/example/riden/activities/LoginActivity.java
+++ app/src/main/java/com/example/riden/activities/LoginActivity.java
@@ -27,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
         etUsername = findViewById(R.id.etUsername);
 //        etPhoneNumber = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
--- app/src/main/java/com/example/riden/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/riden/fragments/ProfileFragment.java
@@ -1,60 +1,77 @@
 package com.example.riden.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.riden.R;
+import com.example.riden.models.User;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
+    ImageButton ibProfileImage;
+    TextView tvUsername;
+    TextView tvFullName;
+    TextView tvCar;
+    ImageView ivCarImage;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    public User user = (User) User.getCurrentUser();
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ibProfileImage = view.findViewById(R.id.ibProfileImage);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvFullName = view.findViewById(R.id.tvFullName);
+        tvCar = view.findViewById(R.id.tvCar);
+        ivCarImage = view.findViewById(R.id.ivCar);
+
+        ibProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Set the onClick event
+            }
+        });
+
+        user.fetchInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                user = (User) object;
+                displayUserInfo();
+            }
+        });
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private void displayUserInfo() {
+        tvUsername.setText(user.getUsername());
+        tvFullName.setText(user.getFullName());
+
+        ParseFile profileImage = user.getProfileImage();
+        ParseFile carImage = user.getCarImage();
+        if(profileImage != null) {
+            Glide.with(getContext()).load(profileImage.getUrl()).circleCrop().into(ibProfileImage);
         }
+        if(carImage != null) {
+            Glide.with(getContext()).load(carImage.getUrl()).into(ivCarImage);
+        }
+
     }
 
     @Override
@@ -62,5 +79,7 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
     }
 }
